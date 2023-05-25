943726dd698b9912b60c6e087d8a397a4eeef48e
YaGRRusso <yagrrusso@gmail.com>
2023-05-25 01:16:41 -0300

    pequenas melhorias

--- util.js
+++ util.js
@@ -5,19 +5,36 @@
 // Version: 2021.03.08
 //
 
-// Function strtoarray
-// Convert strings to array of type
-const strtoarray = (str,type) => {
-    let result = str.split(" ")
-    
-    if (type == "Number") {
-        result = result.map(Number)
-    }
+/**
+ * Convert string into array of dynamic values
+ * @param {string} str String that will be converted
+ * @param {string} split Split string into array
+ * @param {"string" | "number" | undefined} type Type of input that will be inserted (optional)
+ * @returns Converted array
+ */
+export const strtoarray = (str, type, split) => {
+  let array = str.split(split);
+  let result = [];
 
-    return result
-}
+  switch (type) {
+    case "number":
+      result = array.map((item) => Number(item));
+      break;
 
-const strtonum = (str) => parseFloat(str)
+    case "string":
+      result = array;
+      break;
 
+    default:
+      array.forEach((item) => {
+        if (/^[0-9]+$/.test(item)) {
+          result = [...result, Number(item)];
+        } else {
+          result = [...result, item];
+        }
+      });
+      break;
+  }
 
-export { strtoarray, strtonum }
+  return result;
+};
